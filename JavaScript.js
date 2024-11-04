8db30395104d3ca34ae31d8f3f0524f559b0dd8a
Xevoz_ <Niklas.loev@gmail.com>
2024-11-04 13:12:59 +0100

    opdatering CardAmount tilføjet

--- JavaScript.js
+++ JavaScript.js
@@ -3,7 +3,15 @@ document.addEventListener('DOMContentLoaded', (event)=> {
     const packName = document.getElementById ('packName');
     const cardName = document.getElementById ('cardName');
     const cardPrice = document.getElementById ('cardPrice');
+    const cardAmount = document.getElementById ('cardAmount');
     const cardList = document.getElementById ('cardList');
+
+    const predefinedData = {
+        packName: 'Obsidian Flames',
+        cardName: 'Pikachu',
+        cardPrice: 1,
+        cardAmount: 1,
+    };
 
     packName.placeholder = predefinedData.packName;
     cardName.placeholder = predefinedData.cardName;
@@ -13,16 +21,17 @@ document.addEventListener('DOMContentLoaded', (event)=> {
         const packName = packName.value.trim() || predefinedData.packName;
         const cardName = cardName.value.trim() || predefinedData.cardName;
         const cardPrice = cardPrice.value.trim() || predefinedData.cardPrice;
+        const cardAmount = cardAmount.value.trim() || predefinedData.cardAmount;
     
         //Bestemmer værdierne for inputtet - hvis ikke korrekt input gives, exekveres der ikke.
-        if (packName !== "" && cardName !== "" && cardPrice !== "") {
+        if (packName !== "" && cardName !== "" && cardPrice !== "" && cardAmount !== "") {
 
             //Her tjekkes om variablerne Brand, Price og Quality stringsne ikke mangler input
             const li = document.createElement('li');
 
             //Hvis input er korrekt og data er udfyldt for strings
             //bliver der created et nyt element med angiven data.
-            li.textContent = `Pack Name: ${predefinedData.packName}, Card Name: ${cardName}, Card Price: ${cardPrice}`;
+            li.textContent = `Pack Name: ${predefinedData.packName}, Card Name: ${cardName}, Card Price: ${cardPrice}, Card Amount: ${cardAmount}`;
             
             //her "printes" det nye Element ind på cardList
             cardList.appendChild(li);
@@ -30,25 +39,27 @@ document.addEventListener('DOMContentLoaded', (event)=> {
             packName.value = "";
             cardName.value = "";
             cardPrice.value = "";
+            cardAmount = "";
 
         } else {
             alert("Please enter all Card details.");
         }
     });
+        //InputValue tjekker om værdien i input feltet er tomt/mellemrum
+        //Trim() fjerner eventuelle start/slut mellemrums tegn
+        if (inputValue.trim() !== "") {
 
-    if (inputValue.trim() !== "") {
+            //her laves vores liste + document.createElement oprette ny node
+            const li = document.createElement('li');
+                //angivet tekst/værdi indsættes i listen til værdien af inputvalue
+                li.textContent = inputValue;
 
-        //her laves vores liste + document.createElement oprette ny node
-        const li = document.createElement('li');
-            //angivet tekst/værdi indsættes i listen til værdien af inputvalue
-            li.textContent = inputValue;
+            cardList.appendChild(li);
 
-        woodPelletList.appendChild(li);
-        woodPelletInput.value = ""; // clear the input after adding
-    }
-     else 
-    {
-        alert("Please enter Card details.");
-    }
+        }
+        else 
+        {
+            alert("Please enter Card details.");
+        }
 });
 
